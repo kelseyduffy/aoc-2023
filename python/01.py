5802fae5318b8ebba010b08e19ab4fc79b285d17
Kelsey Duffy <kelseyduffy@protonmail.com>
2023-12-01 18:06:47 -0500

    day 1 part 2 done

--- python/01.py
+++ python/01.py
@@ -11,3 +11,53 @@ for string in strings:
     total_calibration += int(just_numbers[-1])
 
 print(total_calibration)
+# answer: 55123
+
+number_words = [
+    ('twone','21'),
+    ('eightwo','82'),
+    ('oneight','18'),
+    ('one', '1'),
+    ('two', '2'),
+    ('three', '3'),
+    ('four', '4'),
+    ('five', '5'),
+    ('six', '6'),
+    ('seven', '7'),
+    ('eight', '8'),
+    ('nine', '9'),
+    ('ten', '10'),
+    ('eleven', '11'),
+    ('twelve', '12'),
+    ('thirteen', '13'),
+    ('fourteen', '14'),
+    ('fifteen', '15'),
+    ('sixteen', '16'),
+    ('seventeen', '17'),
+    ('eighteen', '18'),
+    ('nineteen', '19'),
+    ('twenty', '20'),
+    ('thirty', '30'),
+    ('forty','40'),
+    ('fifty','50'),
+    ('sixty','60'),
+    ('seventy','70'),
+    ('eighty','80'),
+    ('ninety','90'),
+]
+
+total_calibration = 0
+
+for string in strings:
+    replaced = string
+    for (word, number) in number_words:
+        replaced = replaced.replace(word, number)
+
+    just_numbers = [char for char in replaced if char.isdigit()]
+    total_calibration += 10 * int(just_numbers[0])
+    total_calibration += int(just_numbers[-1])
+
+print(total_calibration)
+# 54851 is too low
+# 55211 is too low
+# answer: 55260
