493dce120e6229d86c2a039cb7780222d47518d3
Kelsey Duffy <kelseyduffy@protonmail.com>
2023-12-02 11:06:24 -0500

    day 2 part 2 done

--- python/02.py
+++ python/02.py
@@ -10,8 +10,15 @@ limits = {
 }
 
 total_ids = 0
+total_powers = 0
 
 for game in games:
+
+    mins = {
+        'red': 0,
+        'blue': 0,
+        'green': 0
+    }
     
     ruled_out = False
 
@@ -25,11 +32,19 @@ for game in games:
         for count in counts:
             (num, color) = count.strip().split(' ')
 
+            if int(num) > mins[color]:
+                mins[color] = int(num)
+
             if int(num) > limits[color]:
                 ruled_out = True
 
     if not ruled_out:
         total_ids += int(id.strip().split(' ')[1])
 
+    total_powers += mins['blue'] * mins['green'] * mins['red']
+
 print(total_ids)
 # answer: 2593
+
+print(total_powers)
+# answer: 54699
